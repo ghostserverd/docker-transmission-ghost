7676f148f96ca4002cdd25604ce6edcd46743d73
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2019-04-12 19:42:31 -0700

    clean up curl command for triggering filebot

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -8,4 +8,8 @@ ARG_LABEL="N/A"
 # Configuration
 CONFIG_OUTPUT="/media"
 
-curl http://filebot:7676/amc?name=${ARG_NAME}&path=${ARG_PATH}&label=${ARG_LABEL}
+curl \
+    --data-urlencode "name=${ARG_NAME}" \
+    --data-urlencode "path=${ARG_PATH}" \
+    --data-urlencode "label=${ARG_LABEL}" \
+    http://filebot:7676/amc
