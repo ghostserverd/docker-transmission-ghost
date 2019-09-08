0379af1d4b89c48233ea8604f2bbb6526ee16dbd
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2019-09-07 20:24:42 -0700

    remove unnecessary "

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -11,9 +11,9 @@ FILEBOT_PORT=${FILEBOT_PORT:-7676}
 
 FILEBOT_CMD=$(\
 echo curl \
-    --data-urlencode "name=\"${ARG_NAME}\"" \
-    --data-urlencode "path=\"${ARG_PATH}\"" \
-    --data-urlencode "label=\"${ARG_LABEL}\"" \
+    --data-urlencode name=\"${ARG_NAME}\" \
+    --data-urlencode path=\"${ARG_PATH}\" \
+    --data-urlencode label=\"${ARG_LABEL}\" \
     http://filebot:${FILEBOT_PORT}/amc)
 
 echo $FILEBOT_CMD >> /config/filebot.log
