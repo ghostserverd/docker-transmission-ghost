4d1d777abcf6dfaba8cb5585a5aeee2851c04ac2
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2020-07-20 22:24:35 -0700

    add support for triggering PVR refresh thanks to Michsior14

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -8,6 +8,8 @@ ARG_LABEL="N/A"
 # Configuration
 CONFIG_OUTPUT="/media"
 FILEBOT_PORT=${FILEBOT_PORT:-7676}
+SONARR_CATEGORY=${SONARR_CATEGORY:-"sonarr"}
+RADARR_CATEGORY=${RADARR_CATEGORY:-"radarr"}
 
 FILEBOT_CMD=$(\
 echo curl \
@@ -18,3 +20,29 @@ echo curl \
 
 echo $FILEBOT_CMD >> /config/filebot.log
 eval $FILEBOT_CMD
+
+REFRESH_NAME=""
+REFRESH_URL=""
+
+case $TR_TORRENT_DIR in
+    *$SONARR_CATEGORY*)
+        REFRESH_NAME="RescanSeries"
+        REFRESH_URL="http://sonarr:${SONARR_PORT}/api/command?apikey=${SONARR_API_KEY}"
+    ;;
+
+    *$RADARR_CATEGORY*)
+        REFRESH_NAME="RescanMovie"
+        REFRESH_URL="http://radarr:${RADARR_PORT}/api/command?apikey=${RADARR_API_KEY}"
+    ;;
+esac
+
+if [ $REFRESH_URL != "" ]; then
+    REFRESH_CMD=$(\
+        echo curl \
+            -d \"{\\\"name\\\":\\\"${REFRESH_NAME}\\\"}\" \
+            -H \"Content-Type: application/json\" \
+	    -X POST \
+            ${REFRESH_URL})
+    echo $REFRESH_CMD >> /config/pvr-refresh.log
+    eval $REFRESH_CMD
+fi
